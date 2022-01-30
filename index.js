d9902907b9f76d16461af9d81fb303093471c61b
Andrei Prakapovich <andre1121311@gmail.com>
2022-01-30 21:06:00 +0100

    Create function setErrorFor and setSuccessFor, showModal and closeModal

--- index.js
+++ index.js
@@ -42,3 +42,16 @@ const checkInputPhone = ()=>{
 		setSuccessFor(inputPhone);
 	}
 }
+
+function setErrorFor(input, message ) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'container-label error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'container-label success';
+}
+
